e15c24da9ea6a6909645041918d87284c9aa0035
S.W.A.T. Robotics <frc1806@gmail.com>
2020-02-08 15:09:59 -0600

    WIP Code with GoalPipeline

---
+++ build/CMakeFiles/electric_eye.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/electric_eye.dir/main.cpp.o"
+  "electric_eye.pdb"
+  "electric_eye"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/electric_eye.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
